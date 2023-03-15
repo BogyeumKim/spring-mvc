ed353da5a72b8aa6b1f080cb49629cb6649d552c
BogyeumKim <54786369+BogyeumKim@users.noreply.github.com>
2023-03-15 18:12:33 +0900

    서블릿 생성 및 출력

--- src/main/java/hello/servlet/basic/HelloServlet.java
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -7,8 +7,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "helloServlet" ,urlPatterns="/hello")
-public class HelloServlet extends HttpServlet {
+@WebServlet(name = "helloServlet" ,urlPatterns="/hello") // 서블릿 애노테이션 , name 무관하며 urlPatterns은 get매핑주소
+public class HelloServlet extends HttpServlet { // 서블릿 이용시 상속받는 HttpServlet
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
@@ -16,11 +16,11 @@ public class HelloServlet extends HttpServlet {
         System.out.println("request = " + request);
         System.out.println("response = " + response);
 
-        String username = request.getParameter("username");
+        String username = request.getParameter("username"); // 요청받은 http 쿼리 파라미터와 매칭
         System.out.println("username = " + username);
 
         response.setContentType("text/plagin");
         response.setCharacterEncoding("utf-8");
-        response.getWriter().write("hello "+ username);
+        response.getWriter().write("hello "+ username); // 리스판스바디에 뿌려줌
     }
 }
