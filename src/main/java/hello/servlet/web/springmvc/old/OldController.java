5f9d0ba79352d4bf528455241d8274c1e3d8ea98
BogyeumKim <54786369+BogyeumKim@users.noreply.github.com>
2023-03-17 17:30:36 +0900

    스프링MVC 핸들러 매핑과 핸들러 어댑터
    Bean Name으로 Mapping 시키고 어댑터를 찾아 실행
    예전방법으로 지금은 @RequestMapping 사용

---
+++ src/main/java/hello/servlet/web/springmvc/old/OldController.java
@@ -0,0 +1,17 @@
+package hello.servlet.web.springmvc.old;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component("/springmvc/old-controller") // 스프링이 알아서 Bean이름으로 핸들러 Mapping 및 어댑터를 찾아실행해줌 => url패턴을 BeanName으로 찾아낸다고 생각
+public class OldController implements Controller {
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        System.out.println("OldController.handleRequest");
+        return null;
+    }
+}
